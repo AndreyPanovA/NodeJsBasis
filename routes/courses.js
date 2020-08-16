fc702a1a12d1a163e07629972368b75199067c70
AndreyPanovA <panov3107@mail.ru>
2020-08-17 01:55:04 +0300

    added couple courses

--- routes/courses.js
+++ routes/courses.js
@@ -1,9 +1,12 @@
 const { Router } = require("express")
+const Course = require("../models/course")
 const router = Router()
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+    const courses = await Course.getAll()
     res.render("courses", {
         title: "Курсы",
-        isCourses: true
+        isCourses: true,
+        courses
     })
     // res.status(200).sendFile(path.join(__dirname, "views", "about.html"))
 })
